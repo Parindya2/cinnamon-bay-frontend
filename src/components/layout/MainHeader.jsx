c2ed4c3c6f3c3e4b02b8578bc1b4be6e0a59be20
Parindya2 <168643692+Parindya2@users.noreply.github.com>
2025-08-09 22:41:38 +0530

    feat(layout): add main header banner with animated overlay text

---
+++ src/components/layout/MainHeader.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+const MainHeader = () => {
+	return (
+		<header className="header-banner">
+			<div className="overlay"></div>
+			<div className="animated-texts overlay-content">
+				<h1>Experience Serenity at Cinnamon Bay</h1>
+			</div>
+		</header>
+	)
+}
+
+export default MainHeader
