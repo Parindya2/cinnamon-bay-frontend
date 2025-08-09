1836495e4c307e8adf9a0bbc26b2810d2ba16c98
Parindya2 <168643692+Parindya2@users.noreply.github.com>
2025-08-09 22:39:41 +0530

    feat(home): add main landing page with carousel, parallax, and service sections

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import MainHeader from '../layout/MainHeader'
+import HotelService from '../common/HotelService'
+import Parallax from '../common/Parallax'
+import RoomCarousel from '../common/RoomCarousel'
+import RoomSearch from '../common/RoomSearch'
+import { useLocation } from "react-router-dom"
+import CinnamonBayIntro from '../common/CinnamonBayIntro'
+
+
+const Home = () => {
+  const location = useLocation();
+  const message = location.state && location.state.message;
+  const currentUser = localStorage.getItem("userId");
+  return (
+    <section>
+      {message && <p className='text-warning px-5'>{message}</p>}
+      {currentUser && <h6 className='text-success text-center'>You are logged-in as{currentUser}</h6>}
+      <MainHeader />
+
+      <section className='container-fluid px-3'>
+        <RoomSearch />
+        <RoomCarousel />
+         <Parallax />
+         <CinnamonBayIntro />
+         <HotelService />
+         <Parallax />
+         
+      </section>
+    </section>
+  )
+}
+
+export default Home
