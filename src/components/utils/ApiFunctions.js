c184751bb3283e160e4648c079e3c03dad2158b9
Parindya2 <168643692+Parindya2@users.noreply.github.com>
2025-08-28 23:18:55 +0530

    fix: temporary change

--- src/components/utils/ApiFunctions.js
+++ src/components/utils/ApiFunctions.js
@@ -5,8 +5,12 @@ import { ca } from "date-fns/locale";
   //  baseURL : "http://localhost:8080"
 //})
 
+//export const api = axios.create({
+//    baseURL: process.env.REACT_APP_API_URL || "http://localhost:8080"
+//})
+
 export const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || "http://localhost:8080"
+    baseURL: "https://cinnamon-bay-backend-production.up.railway.app"
 })
 
 // Debug logs 
